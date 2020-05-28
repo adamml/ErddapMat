b09dcf4ef7eb13986aa2b9228ef80e5bf4264244
Adam Leadbetter <adamml@users.noreply.github.com>
2020-05-28 16:46:11 +0100

    Update ErddapMat.m

--- ErddapMat.m
+++ ErddapMat.m
@@ -1,5 +1,9 @@
 classdef ErddapMat
-%ErddapMat is a Matlab/Octave class providing a client to NOAA's Erddap server
+  %ErddapMat is a Matlab/Octave class providing a client to NOAA's Erddap server
+  %
+  % ErddapMat Methods:
+  %     getDatasetsOnServer(serverBaseUrl) - List datasets on an Erddap server
+  %     suggestServers() - Suggest a list of known Erddap servers
 
 %-------------------------------------------------------------------------------
 % Private attributes
@@ -19,16 +23,18 @@ classdef ErddapMat
   methods
     
     function serverList = suggestServers(obj)
-    %suggestServers returns a list of known Erddap instances
-    %
-    % serverList = suggestServers()
-    %
-    % Returns a list of Errdap servers as curated on the awesome-erddap list.
-    % See https://github.com/IrishMarineInstitute/awesome-erddap for more
-    % details.
-    %
-    % Adam Leadbetter - 2020-May-15
-    %
+      %suggestServers returns a list of known Erddap instances
+      %
+      % suggestServers()
+      %
+      % Returns a list of Errdap servers as curated on the awesome-erddap list.
+      % See https://github.com/IrishMarineInstitute/awesome-erddap for more
+      % details.
+      %
+      %-------------------------------------------------------------------------
+      % Adam Leadbetter - 2020-May-15
+      %
+      
       kvp = urlread(obj._privateAwesomeErddapJSON);
       colonInKvp = strfind(kvp,":");
       %
@@ -55,6 +61,22 @@ classdef ErddapMat
     end
     
     function datasetsOnServer = getDatasetsOnServer(obj, serverBaseUrl)
+    %getDatasetsOnServer lists the datasets on a given Erddap server instance
+    %
+    % getDatasetOnServer(serverBaseUrl)
+    %
+    % Returns a cell array of strings containing the output of the RESTful call 
+    % to an Erddap server's base information page.
+    %
+    % Example
+    % -------
+    %     em = ErddapMat();
+    %     datasets = em.getDatasetsOnServer("http://erddap.marine.ie");
+    %
+    %---------------------------------------------------------------------------
+    % Adam Leadbetter - 28 May 2020
+    %
+    
       if(nargin < 2 || nargin > 3)
         error(cat(2,"\n\tErddapMat -> ERROR -> method @getDatasetsOnServer", ...
                   " requires one input argument - supplied = %i \n"),nargin-1);
@@ -62,10 +84,8 @@ classdef ErddapMat
       if(serverBaseUrl(end) == '/')
         serverBaseUrl(end) = [];
       end
-      display(obj.processCSV(urlread(cat(2,serverBaseUrl,...
-                                    "/erddap/info/index.mat"))));
-      datasetsOnServer = urlread(cat(2,serverBaseUrl,...
-                                    "/erddap/info/index.mat"));
+      datasetsOnServer = obj.processCSV(urlread(cat(2,serverBaseUrl,...
+                                    "/erddap/info/index.csv")));
     end
     
   end
@@ -79,7 +99,34 @@ classdef ErddapMat
   methods (Access = private)
     
     function csvParsed = processCSV(obj, csvString)
-      csvParsed = "";
+    %Parse a CSV string to a cell array
+      splitter = regexp(csvString,...
+                          '(?:,|\n|^)("(?:(?:"")*[^"]*)*"|[^",\n]*|(?:\n|$))');
+      if(splitter(1) == 1)
+        splitter(1)  == [];
+      end
+      %
+      % Find the first new line to identify the number of columns we're
+      % reading
+      %
+      newLines = strfind(csvString,"\n");
+      while(newLines(1) == 1)
+        newLines(1)  =  [];
+      end
+      numberOfColumns = length(strfind(csvString(1:newLines(1)),",")) + 1;
+      csvParsed = {};
+      for(idxCsvSplitter  = 2:1:length(splitter))
+        csvParsed{idxCsvSplitter-1} = ...
+              csvString(splitter(idxCsvSplitter - 1):splitter(idxCsvSplitter));
+        if(csvParsed{idxCsvSplitter-1}(1)  ==  ",")
+          csvParsed{idxCsvSplitter-1}(1) = [];
+        end
+        if(csvParsed{idxCsvSplitter-1}(end)  ==  ",")
+          csvParsed{idxCsvSplitter-1}(end) = [];
+        end
+      end
+      csvParsed = reshape(csvParsed,numberOfColumns,...
+                                            length(csvParsed)/numberOfColumns)';
     end
   
   end
